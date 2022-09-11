9dfd53a6d60e132a325d320b3fe90bc977be6fa8
MTP <vanmanh289vn@gmail.com>
2022-09-11 22:09:52 +0700

    ket noi database

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { Route } from 'core/interfaces';
 import express from 'express';
+import mongoose from 'mongoose';
 
 class App {
     public app: express.Application;
@@ -9,6 +10,7 @@ class App {
         this.app = express();
         this.port = process.env.PORT || 5000;
         this.initializeRoutes(routes);
+        this.connectToDatabase();
     }
 
     public listen() {
@@ -21,6 +23,16 @@ class App {
         routes.forEach((route) => {
             this.app.use('/', route.router);
         })
+    }
+
+    private connectToDatabase(){
+        try {
+            const connectString = "mongodb+srv://tedu:3LcQF9rjCrco3YbA@master.qecbe.mongodb.net/tedu_social?retryWrites=true&w=majority";
+            mongoose.connect(connectString);
+            console.log('Connect to database successful');
+        } catch (error) {
+            console.log('Connect to database error');
+        }
     }
 }
 
