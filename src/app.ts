aebcb208b60922da2f5c9cfd6320bdce1278c86a
MTP <vanmanh289vn@gmail.com>
2022-09-13 23:31:47 +0700

    commit middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,30 @@
 import { Route } from 'core/interfaces';
 import express from 'express';
 import mongoose from 'mongoose';
+import hpp from 'hpp';
+import morgan from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
+import { Logger } from './core/utils';
 
 class App {
     public app: express.Application;
     public port: string | number;
+    public production: boolean;
 
     constructor(routes: Route[]) {
         this.app = express();
         this.port = process.env.PORT || 5000;
+        this.production = process.env.NODE_ENV == 'production' ? true : false;
+
         this.initializeRoutes(routes);
         this.connectToDatabase();
+        this.initializeMiddleware();
     }
 
     public listen() {
         this.app.listen(this.port, () => {
-            console.log(`Server is listening on port ${this.port}`);
+            Logger.info(`Server is listening on port ${this.port}`);
         });
     }
 
@@ -25,18 +34,28 @@ class App {
         })
     }
 
-    private connectToDatabase(){
-        try {
-            const connectString = process.env.MONGODB_URI;
-            if(!connectString){
-                console.log('Connection string is invalid');
-                return;
-            }
-            mongoose.connect(connectString);
-            console.log('Connect to database successful');
-        } catch (error) {
-            console.log('Connect to database error');
+    private initializeMiddleware() {
+        if(this.production){
+            this.app.use(hpp());
+            this.app.use(helmet());
+            this.app.use(morgan('combined'));
+            this.app.use(cors({ origin: 'your.domain.com', credentials: true }));
+        } else {
+            this.app.use(morgan('dev'));
+            this.app.use(cors({ origin: true, credentials: true }));
         }
+    }
+
+    private connectToDatabase(){
+        const connectString = process.env.MONGODB_URI;
+        if(!connectString){
+            Logger.error('Connection string is invalid');
+            return;
+        }
+        mongoose.connect(connectString).catch((reason) => {
+            Logger.error(reason);
+        });
+        Logger.info('Connect to database successful');
     }
 }
 
