007a626864e1fa546d97296cbaea7fd21e1077a0
MTP <vanmanh289vn@gmail.com>
2022-09-18 16:35:07 +0700

    debug in Visual studio code

--- src/modules/index/index.controller.ts
+++ src/modules/index/index.controller.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from "express"
 export default class IndexController {
     public index = (req: Request, res: Response, next: NextFunction) => {
         try {
-            res.status(200).send('API is running...MTP Controller Okie lession 17');
+            res.status(200).send('API is running...MTP Controller Okie lession 20');
         } catch (error) {
             next(error);
         }
